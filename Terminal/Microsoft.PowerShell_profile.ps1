412222e61aa4189c018ae783da6e832dcbf10444
Renato Conrado <renatoconrado38@gmail.com>
2024-06-12 15:17:27 -0300

    Update

---
+++ Terminal/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+# Initialize oh-my-posh with a specific theme
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/atomic.omp.json' | Invoke-Expression
+
+# Import Terminal-Icons module
+Import-Module -Name Terminal-Icons
